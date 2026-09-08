26adfb745cd0e5391298886bcf59c6c992ece170
Thuthu <arthur.mfernandes@yahoo.com.br>
2026-09-07 22:24:12 -0300

    Added transformation from xlsx to Parquet

--- etl/transform/transformation.py
+++ etl/transform/transformation.py
@@ -28,3 +28,28 @@ year_file = date.today().year -1
 while read_file_transform_csv(f'raw/ssp_vehicles_stolen_file_{year_file}.xlsx', year_file) == 'success':
     year_file = year_file - 1
 
+
+
+#%%
+import duckdb as dudb
+from datetime import date
+
+def read_file_transform_parquet(file_name_input: str, year_input: int) -> str:
+    print(file_name_input)
+    try:
+        df_excel = dudb.sql(f"COPY(select * from read_xlsx({file_name_input}, sheet='VEICULO_{year_input}') TO '{file_name_input}.parquet' (FORMAT parquet);")
+        return 'success'
+    except Exception as e:
+        print(e)
+        if str(e).find(f"Worksheet named VEICULO_{year_input} not found"):
+            try:
+                df_excel = dudb.sql(f"COPY(select * from read_xlsx({file_name_input}, sheet='VEICULOS_{year_input}') TO '{file_name_input}.parquet' (FORMAT parquet);")
+                return 'success'
+            except Exception as e:
+                return print(e)
+        return print(e)
+
+year_file = date.today().year -1
+while read_file_transform_parquet(f'raw/ssp_vehicles_stolen_file{year_file}.xlsx', year_file) == 'success':
+    year_file = year_file - 1
+
